ef9874c995d587add71173bef0cb94c7b559ee36
Homuncoli <dan18765@spengergasse.at>
2021-11-19 08:45:17 +0100

    feat: add simple performance test

--- Entra.cpp
+++ Entra.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <chrono>
 
 #include "Entra.h"
+
+using std::chrono::high_resolution_clock;
+using std::chrono::duration_cast;
+using std::chrono::duration;
+using std::chrono::milliseconds;
 
 struct Transform {
     float x = 1,y,z;
@@ -16,43 +22,50 @@ struct Rotation {
     float pitch, yaw, roll;
 };
 
-// ToDo: do not allow dublicates
-// ToDo: support multiple components per system
-class TransformationSystem : public Entra::System<Transform> {
+class TransformationSystem : public Entra::System<Transform, Rotation> {
+    private:
+        float gravity;
     public:
-        explicit TransformationSystem(Entra::Registry* pRegistry, std::string a): Entra::System<Transform>(pRegistry) {
-            std::cout << a << std::endl;
-        }
+        explicit TransformationSystem(Entra::Registry* pRegistry, float g): Entra::System<Transform, Rotation>(pRegistry), gravity(g) {}
+        
         void update(double deltaTime) {
             for (auto component : components) {
-                std::cout << std::get<0>(component)->x << std::endl;
+                for (int i=0; i<20; i++)
+                    std::get<0>(component)->y *= gravity * deltaTime;
             }
         }
 };
 
-// ToDo: Systems need to to be able to have multiple components they care about
 int main() {
-    Entra::Registry registry;
-    registry.addSystem<TransformationSystem>("AAAAAAAAA");
+    constexpr size_t N_ENTITIES = 10000;
+    constexpr size_t N_CYCLES = 100;
+    
+    auto start = high_resolution_clock::now();
+    Entra::Registry* registry = new Entra::Registry();
+    auto end = high_resolution_clock::now();
+    std::cout << "registry creation: " << duration_cast<milliseconds>(end - start).count() << "ms" << std::endl;
 
-    Entra::EntityId id1; 
-    Entra::EntityId id2; 
-    Entra::EntityId id3; 
+    start = high_resolution_clock::now();
+    registry->addSystem<TransformationSystem>(9.81f);
+    end = high_resolution_clock::now();
+    std::cout << "system creation: " << duration_cast<milliseconds>(end - start).count() << "ms" << std::endl;
 
-    std::cout << "Create new entity: " << (id1 = registry.createEntity()) << std::endl;
-    std::cout << "Create new entity: " << (id2 = registry.createEntity()) << std::endl;
-    std::cout << "Create new entity: " << (id3 = registry.createEntity()) << std::endl;
-    registry.addComponent<Transform>(id1, 1, 2, 3);
-    registry.addComponent<Transform>(id2, 4, 5, 6);
-    registry.addComponent<Rotation>(id2);
-    registry.addComponent<Rotation>(id3);
-    std::cout << "====================" << std::endl;
+    start = high_resolution_clock::now();
+    for (int i=0; i<N_ENTITIES; i++) {
+        Entra::EntityId id = registry->addEntity();
+        registry->addComponent<Transform>(id, 0, 0, 0);
+        registry->addComponent<Rotation>(id);
+    }
+    end = high_resolution_clock::now();
+    std::cout << N_ENTITIES << " entities creation: " << duration_cast<milliseconds>(end - start).count() << "ms" << std::endl;
 
-    std::cout << registry.getComponent<Transform>(id1)->x << std::endl;
-    std::cout << registry.getComponent<Transform>(id2)->x << std::endl;
-    std::cout << "====================" << std::endl;
-    registry.update(0.2);
-    std::cout << "====================" << std::endl;
-
+    start = high_resolution_clock::now();
+    for (int i=0; i<N_CYCLES; i++) {
+        registry->update(0.1f);
+    }
+    end = high_resolution_clock::now();
+    std::cout << N_CYCLES << " updates: " << duration_cast<milliseconds>(end - start).count() << "ms" << std::endl;
+    
+    delete registry;
     return 0;
 }
